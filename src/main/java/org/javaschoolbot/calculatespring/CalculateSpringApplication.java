47487405a051a011ad5d4be08719ef7e5b5b7ca8
PryadkoBogdan <bogdanpryadko1@gmail.com>
2024-02-11 10:58:27 +0300

    first commit

---
+++ src/main/java/org/javaschoolbot/calculatespring/CalculateSpringApplication.java
@@ -0,0 +1,13 @@
+package org.javaschoolbot.calculatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculateSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaschoolbot/calculatespring/CalculateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaschoolbot.calculatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
